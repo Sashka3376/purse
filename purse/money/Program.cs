d8417d043412c74bca2fcf1b8b9a47f7bdeb9640
orysiayashchun <54995877+orysiayashchun@users.noreply.github.com>
2022-03-29 11:52:30 +0300

    create money

---
+++ purse/money/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace money
+{
+    class Money
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Money");
+        }
+    }
+}
